1d635dff94ed4b0411e05edd15c95f03ba9d8482
DSVarga <varga.andreas@gmail.com>
2023-05-28 12:46:28 +0200

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,19 +4,19 @@ using Test, FaultDetectionTools
 
 @testset "Test FaultDetectionTools" begin
 # test FDI tools
-# include("test_fdiutils.jl")
-# # test FDI analysis tools
-# include("test_fdigenspec.jl")
-# # test FDI synthesis functions
-# include("test_efdsyn.jl")
-# include("test_efdisyn.jl")
+include("test_fdiutils.jl")
+# test FDI analysis tools
+include("test_fdigenspec.jl")
+# test FDI synthesis functions
+include("test_efdsyn.jl")
+include("test_efdisyn.jl")
 include("test_afdsyn.jl")
-# include("test_afdisyn.jl")
-# include("test_emmsyn.jl")
-# include("test_ammsyn.jl")
+include("test_afdisyn.jl")
+include("test_emmsyn.jl")
+include("test_ammsyn.jl")
 # test model detection functions
 include("test_emdsyn.jl")
-# include("test_amdsyn.jl")
+include("test_amdsyn.jl")
 end
 
 end
